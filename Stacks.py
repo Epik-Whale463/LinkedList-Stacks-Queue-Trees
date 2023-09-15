351f45e5a7acc76c6d2cb9466089950cdb3fdb76
Epik-Whale463 <122500108+Epik-Whale463@users.noreply.github.com>
2023-09-15 22:19:34 +0530

    Add files via upload
    Added Stacks

--- Stacks.py
+++ Stacks.py
@@ -1,63 +1,47 @@
-# Python program to demonstrate
-# stack implementation using a linked list.
-# node class
+class FixedSizeStack:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.stack = [None] * capacity  # Create an empty list with the specified capacity
+        self.size = 0  # Current size of the stack
 
-class Node:
-	def __init__(self, value):
-		self.value = value
-		self.next = None
+    
 
+    def push(self, item):
+        if self.is_full():
+            print("Stack is full. Cannot push element.")
+            return
+        self.stack[self.size] = item
+        self.size += 1
 
-class Stack:
+    def pop(self):
+        if self.is_empty():
+            print("Stack is empty. Cannot pop element.")
+            return None
+        self.size -= 1
+        popped_item = self.stack[self.size]
+        self.stack[self.size] = None  # Remove the reference to the popped item
+        return popped_item
 
-	# Initializing a stack.
-	# Use a dummy node, which is
-	# easier for handling edge cases.
-	def __init__(self):
-		self.head = Node("head")
-		self.size = 0
+    def peek(self):
+        if self.is_empty():
+            print("Stack is empty. Cannot peek element.")
+            return None
+        return self.stack[self.size - 1]
 
-	# String representation of the stack
-	def __str__(self):
-		cur = self.head.next
-		out = ""
-		while cur:
-			out += str(cur.value) + "->"
-			cur = cur.next
-		return out[:-2]
+    def display(self):
+        if self.is_empty():
+            print("Stack is empty.")
+            return
+        for i in range(self.size - 1, -1, -1):
+            print(self.stack[i])
 
-	# Get the current size of the stack
-	def getSize(self):
-		return self.size
+    def get_size(self):
+        return self.size
 
-	# Check if the stack is empty
-	def isEmpty(self):
-		return self.size == 0
+    def is_empty(self):
+        return self.size == 0
 
-	# Get the top item of the stack
-	def peek(self):
-
-		# Sanitary check to see if we
-		# are peeking an empty stack.
-		if self.isEmpty():
-			raise Exception("Peeking from an empty stack")
-		return self.head.next.value
-
-	# Push a value into the stack.
-	def push(self, value):
-		node = Node(value)
-		node.next = self.head.next
-		self.head.next = node
-		self.size += 1
-
-	# Remove a value from the stack and return.
-	def pop(self):
-		if self.isEmpty():
-			raise Exception("Popping from an empty stack")
-		remove = self.head.next
-		self.head.next = self.head.next.next
-		self.size -= 1
-		return remove.value
-
+    def is_full(self):
+        return self.size == self.capacity
 
 
